4cb9ca7f02f25ee4e7e859695f136a0025662d05
johnson777777 <127622973+johnson777777@users.noreply.github.com>
2024-06-27 20:28:30 +0800

    change dir

--- class_1_html/httpserver.py
+++ class_1_html/httpserver.py
@@ -2,22 +2,19 @@ import sys
 import http.server
 from http.server import SimpleHTTPRequestHandler
 import socket
+import os
 
 HandlerClass = SimpleHTTPRequestHandler
 ServerClass  = http.server.HTTPServer
 Protocol     = "HTTP/1.0"
 
-if sys.argv[1:]:
-    port = int(sys.argv[1])
-else:
-    port = 80
-server_address = ('0.0.0.0', port)
+server_address = ('0.0.0.0', 8000)
+
+os.chdir(os.path.dirname(__file__))
 
 HandlerClass.protocol_version = Protocol
 httpd = ServerClass(server_address, HandlerClass)
 
-sa = httpd.socket.getsockname()
-# print("Serving HTTP on", sa[0], "port", sa[1], "...")
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 ip = s.getsockname()[0]
