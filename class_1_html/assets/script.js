1ab657656aca13d46bc1fef1b5dad1fc6fad1f42
johnson777777 <127622973+johnson777777@users.noreply.github.com>
2024-07-02 03:05:35 +0800

    delete

--- class_1_html/assets/script.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.getElementById("messageForm").addEventListener('submit', function(event) {
-//         event.preventDefault();
-//         const name = document.getElementById('name').value;
-//         const message = document.querySelector('textarea[id="message"]').value;
-//         alert(name + '說' + message);
-//     });
-// });
