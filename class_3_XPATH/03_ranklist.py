680d751b861357be636aecbacbf49ba580e9baab
Bill-Huangz <66486446+Bill-Huangz@users.noreply.github.com>
2024-07-04 11:47:13 +0800

    Add files via upload

---
+++ class_3_XPATH/03_ranklist.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import cv2 as cv
+import numpy as np
+import time
+
+url='https://www.holiday.com.tw/SongInfo/SongList.aspx?st=top&lt=tc'
+if __name__=='__main__':
+    service = Service(executable_path="/home/cscamp/chromedriver/chromedriver")
+    driver = webdriver.Chrome(service=service)
+    #driver=webdriver.Chrome()
+    driver.get(url)
+    wait=WebDriverWait(driver, 10)
+    # 爬到所有歌
+    songsXPATH="__???__"
+    songs=wait.until(EC.presence_of_all_elements_located((By.XPATH, songsXPATH)), "Error")
+    for song in songs:
+        print(song.text)
+
+    driver.quit()
